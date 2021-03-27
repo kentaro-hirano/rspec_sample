ad2120966add1d13c13fae855c0f7b917035e3a9
EC2 Default User <yakyukentaro@gmail.com>
2021-03-27 11:47:08 +0000

    add root_path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "food_enquetes#index"
   resources :gym_enquetes
   resources :food_enquetes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
